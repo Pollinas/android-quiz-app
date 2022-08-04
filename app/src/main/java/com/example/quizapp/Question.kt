583ed6476494d68bd7726f84c0d41c112b525df6
paulina szylin <paulina.szylin.programista@gmail.com>
2022-08-04 20:12:19 +0200

    Questions UI setup part two, connected the UI to the question model

--- app/src/main/java/com/example/quizapp/Question.kt
+++ app/src/main/java/com/example/quizapp/Question.kt
@@ -4,9 +4,9 @@ data class Question(
     val id: Int,
     val question: String,
     val image: Int,
-    val OptionOne: String,
-    val OptionTwo: String,
-    val OptionThree: String,
-    val OptionFour: String,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
     val correctAnswer: Int
 )
